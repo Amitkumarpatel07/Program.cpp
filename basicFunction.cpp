b588706919bdbda27b24ed1f31a7174c9ef1831d
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-03-15 20:48:33 +0530

    Create basicFunction.cpp

---
+++ basicFunction.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void usa(){
+     cout<<"You are in USA"<<endl;
+     return;
+     cout<<"You are in USA"<<endl;   
+}
+void india(){
+    cout<<"You are in India"<<endl;
+    return;
+}
+int main (){
+    usa();
+}
